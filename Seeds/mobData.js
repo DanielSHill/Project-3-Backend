6ae91a4119b1cc09d61bcdf9e93893d02b0fdebb
Daniel Hill <daniel.s.hill01@gmail.com>
2024-03-30 14:17:35 -0600

    Initial commit

---
+++ Seeds/mobData.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        name: "Zombie",
+        health: 20,
+        damage: 2.5,
+        armor: 2,
+        image: "https://www.minecraft.net/content/dam/games/minecraft/game-characters/WILBMM_Zombie.png"
+    },
+    {
+        name: "Spider",
+        health: 16,
+        damage: 2,
+        armor: 0,
+        image: "https://preview.redd.it/omxy6o5efse31.png?auto=webp&s=a15a6285b927973b79bf076418b3f6ccf82d2a74",
+    },
+    {
+        name: "Zombified Piglin",
+        health: 20,
+        damage: 5,
+        armor: 2,
+        image: "https://qph.fs.quoracdn.net/main-qimg-7f194339e137e5f727dccae351cfa342",
+    },
+    {
+        name: "Creeper",
+        health: 20,
+        damage: 22.5,
+        armor: 0,
+        image: "https://upload.wikimedia.org/wikipedia/en/4/49/Creeper_%28Minecraft%29.png",
+    },
+    {
+        name: "Skeleton",
+        health: 20,
+        damage: 4,
+        armor: 0,
+        image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/minecraft/b/b6/118px-Skeleton.png?width=500",
+    },
+    {
+        name: "Enderman",
+        health: 40,
+        damage: 4.5,
+        armor: 0,
+        image: "https://lh3.googleusercontent.com/proxy/mg1FMDUAX9XvrxELKBeZr6jX8DElogjR19pcz9pfioccdDhP0f007sTR5SB1O_Jm71K7HsvZ3rpkAo-2efpAdx5zu4e93suodGMv",
+    },
+]
---
+++ server.js
@@ -0,0 +1,121 @@
+///////////////////////////////
+// DEPENDENCIES
+//////////////////////////////
+require('dotenv').config();
+const { DATABASE_URL, PORT = 3001 } = process.env;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+///////////////////////////////
+// DATABASE CONNECTION
+///////////////////////////////
+
+// Establish Connection
+mongoose.connect(DATABASE_URL)
+
+//Connection Events
+mongoose.connection
+    .on('open', () => console.log("Connected to MongoDB"))
+    .on('close', () => console.log("Couldn't connect to MongoDB"))
+    .on('error', () => console.log(error));
+
+
+///////////////////////////////
+// MODELS
+///////////////////////////////
+
+
+const Schema = mongoose.Schema;
+
+const MobsSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    health: { type: Number, required: true },
+    damage: { type: Number, required: true },
+    armor: { type: Number, required: true },
+    image: { type: String, required: false },
+}, { timestamps: true });
+
+const Mobs = mongoose.model('Mobs', MobsSchema)
+
+
+///////////////////////////////
+// MIDDLEWARE
+///////////////////////////////
+
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+///////////////////////////////
+// ROUTES
+///////////////////////////////
+
+// Test Start Route
+app.get('/', (req, res) => {
+    res.send('This is the start page')
+})
+
+// // Mob Seed Route
+// app.get('/seed', (req, res) => {
+//     Mobs.deleteMany({}, (error, allMobs) => {})
+
+//     Mobs.create(mobSeed, (error, data) => {
+//         res.redirect('/mobs')
+//     })
+// })
+
+// Mob Index Route
+app.get('/mobs', async (req, res) => {
+    try {
+        res.json(await Mobs.find({}))
+    } catch (error) {
+
+        res.status(400).json(error);
+    }
+})
+
+
+// Mob Create Route
+app.post('/mobs', async (req, res) => {
+    try {
+        res.json(await Mobs.create(req.body));
+    } catch (error) {
+
+        res.status(400).json(error);
+    }
+})
+
+// Mob Delete Route
+// app.delete('/mobs/:id', async (req, res) => {
+//     try{ 
+//         res.json(await Mobs.findByIdAndDelete(req.params.id));
+//     } catch (error) {
+
+//         res.status(400).json(error);
+//     }
+// })
+
+// Mob Update Route
+app.put('/mobs/:id', async (req, res) => {
+    try {
+        res.json(
+            await Mobs.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        );
+    } catch (error) {
+
+        res.status(400).json(error);
+    }
+})
+
+///////////////////////////////
+// LISTENER
+///////////////////////////////
+
+
+app.listen(PORT, () => console.log(`Port ${PORT} is up and running...`))
